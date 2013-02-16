a26b44c9ed026996331eba049ca9a858762b5249
Paul Fioravanti <paul.fioravanti@gmail.com>
2013-02-17 08:38:51 +1030

    Require all files under support directoryfor testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,6 @@ require 'simplecov'
 SimpleCov.start
 
 require 'rspec'
-# require 'support/utilities'
-# Dir["support/*.rb"].each {|file| require file }
 Dir[File.dirname(__FILE__) + '/support/*.rb'].each {|file| require file }
 
 include ToyRobot
