77f77b2ed252a5ab0c7a44b4047c0280051c700a
Paul Fioravanti <paul.fioravanti@gmail.com>
2013-02-22 10:58:42 +1030

    Attempt to get rid of code duplication smell by moving logic to another file

--- lib/toy_robot/application.rb
+++ lib/toy_robot/application.rb
@@ -112,53 +112,53 @@ module ToyRobot
       end
   end
 
-  # Main application class for extended Toy Robot app
-  class ExtendedApplication < Application
+  # # Main application class for extended Toy Robot app
+  # class ExtendedApplication < Application
 
-    def initialize
-      super
-      define_extended_rules
-    end
+  #   def initialize
+  #     super
+  #     define_extended_rules
+  #   end
 
-    def route(instruction)
-      return "" if instruction == "\n" || instruction == ""
-      response = execute_instruction(instruction)
-      response << "Hint: PLACE robot first.\n" unless placed?
-      response
-    end
+  #   def route(instruction)
+  #     return "" if instruction == "\n" || instruction == ""
+  #     response = execute_instruction(instruction)
+  #     response << "Hint: PLACE robot first.\n" unless placed?
+  #     response
+  #   end
 
-    private
+  #   private
 
-      def execute_instruction(instruction)
-        if parse_instruction(instruction) && valid_robot_command?
-          initialize_extended_world if @command == :place
-          response = @robot.send(@command, *@args)
-        else
-          response = "Invalid Command.\n"
-        end
-      end
+  #     def execute_instruction(instruction)
+  #       if parse_instruction(instruction) && valid_robot_command?
+  #         initialize_extended_world if @command == :place
+  #         response = @robot.send(@command, *@args)
+  #       else
+  #         response = "Invalid Command.\n"
+  #       end
+  #     end
 
-      def initialize_extended_world
-        @board ||= ExtendedBoard.new
-        @robot ||= ExtendedRobot.new(@board)
-      end
+  #     def initialize_extended_world
+  #       @board ||= ExtendedBoard.new
+  #       @robot ||= ExtendedRobot.new(@board)
+  #     end
 
-      def define_extended_rules
-        @permitted_commands += [
-          {
-            name: :block,
-            args_size: 0,
-            conditions: ['placed?']
-          },
-          {
-            name: :map,
-            args_size: 0,
-            conditions: ['placed?']
-          }
-        ]
-        @usage.insert(0,  "*** EXTENDED MODE ***\n")
-        @usage.insert(93, "BLOCK\n"\
-                          "MAP\n")
-      end
-  end
+  #     def define_extended_rules
+  #       @permitted_commands += [
+  #         {
+  #           name: :block,
+  #           args_size: 0,
+  #           conditions: ['placed?']
+  #         },
+  #         {
+  #           name: :map,
+  #           args_size: 0,
+  #           conditions: ['placed?']
+  #         }
+  #       ]
+  #       @usage.insert(0,  "*** EXTENDED MODE ***\n")
+  #       @usage.insert(93, "BLOCK\n"\
+  #                         "MAP\n")
+  #     end
+  # end
 end
