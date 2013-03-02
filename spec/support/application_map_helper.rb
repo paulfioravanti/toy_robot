1f0a1320e2144c53104cb31ca57eaf59a182a4e2
Paul Fioravanti <paul.fioravanti@gmail.com>
2013-03-02 11:43:49 +1030

    Break up test helpers

---
+++ spec/support/application_map_helper.rb
@@ -0,0 +1,11 @@
+# def empty_application_map
+#   "   0   1   2   3   4"
+#   "4 [ ] [ ] [ ] [ ] [ ]\n"
+#   "3 [ ] [ ] [ ] [ ] [ ]\n"
+#   "2 [ ] [ ] [ ] [ ] [ ]\n"
+#   "1 [ ] [ ] [ ] [ ] [ ]\n"
+#   "0 [ ] [ ] [ ] [ ] [ ]\n"
+#   "Robots on the Board:\n"
+#   "None\n"
+#   "Hint: PLACE a robot first.\n"
+# end
---
+++ spec/support/board_map_helper.rb
@@ -0,0 +1,29 @@
+def robot_any_direction_board_map
+  "   0   1   2   3   4\n"\
+  "4 [ ] [ ] [ ] [ ] [ ]\n"\
+  "3 [ ] [ ] [ ] [ ] [ ]\n"\
+  "2 [ ] [ ] [X] [ ] [ ]\n"\
+  "1 [ ] [ ] [ ] [ ] [ ]\n"\
+  "0 [ ] [ ] [ ] [ ] [ ]\n"
+end
+
+def robot_and_blocks_board_map
+  "   0   1   2   3   4\n"\
+  "4 [ ] [X] [ ] [X] [ ]\n"\
+  "3 [X] [ ] [X] [ ] [X]\n"\
+  "2 [ ] [X] [ ] [X] [ ]\n"\
+  "1 [X] [ ] [X] [ ] [X]\n"\
+  "0 [ ] [X] [ ] [X] [X]\n"
+end
+
+# def empty_board_map
+#   "   0   1   2   3   4"
+#   "4 [ ] [ ] [ ] [ ] [ ]\n"
+#   "3 [ ] [ ] [ ] [ ] [ ]\n"
+#   "2 [ ] [ ] [ ] [ ] [ ]\n"
+#   "1 [ ] [ ] [ ] [ ] [ ]\n"
+#   "0 [ ] [ ] [ ] [ ] [ ]\n"
+#   "Occupied Positions:\n"
+#   "None\n"
+#   "Hint: PLACE a robot first.\n"
+# end
