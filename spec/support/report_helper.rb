cd6d4e43810a18bcb46f906994334497e69293a5
Paul Fioravanti <paul.fioravanti@gmail.com>
2013-03-03 14:15:27 +1030

    All tests pass and 100% coverage

---
+++ spec/support/report_helper.rb
@@ -0,0 +1,7 @@
+def robot_2_2_north_report
+  "2,2,NORTH\n"
+end
+
+def extended_robot_2_2_north_report_no_name
+  "R1's Position: 2,2,NORTH\n"
+end
---
+++ spec/support/response_helper.rb
@@ -0,0 +1,7 @@
+def post_place_invalid_response
+  "Invalid Command.\n"
+end
+
+def pre_place_invalid_response
+  "Invalid Command.\nHint: PLACE a robot first.\n"
+end
