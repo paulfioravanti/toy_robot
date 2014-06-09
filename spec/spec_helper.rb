4774ccb0af92f17ed6a7de58552af51a231c65fb
Paul Fioravanti <paul.fioravanti@gmail.com>
2014-06-09 10:39:31 +1000

    Explicitly enable should cause I don't currently have time to redo everything

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,4 +4,10 @@ require 'rspec'
 require 'rspec/its'
 Dir[File.dirname(__FILE__) + '/support/*.rb'].each {|file| require file }
 
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+end
+
 include ToyRobot
